31849ff023874815b5db777aa25d91ad0ce5d3ba
Carlos Ignacio Pérez Sechi <secci.jr@gmail.com>
2014-09-15 23:31:54 +0200

    Added som behavioral to integration test

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -1,6 +1,5 @@
 import org.junit.runner._
 import org.specs2.runner._
-import play.api.i18n._
 import org.specs2.mutable.Specification
 import org.openqa.selenium.phantomjs.PhantomJSDriverService
 
@@ -10,28 +9,17 @@ import org.openqa.selenium.phantomjs.PhantomJSDriverService
 @RunWith(classOf[JUnitRunner])
 class IntegrationSpec extends Specification {
 
-  val enUSLangCode = "en-US"
-  val ptBRLangCode = "pt-BR"
-
-  val enUSOptions = getPhantomJSLanguageOption(enUSLangCode)
-  val ptBROptions = getPhantomJSLanguageOption(ptBRLangCode)
-
   "Application" should {
 
-    "work from within a browser with en-US language" in new WithPhantomJS(enUSOptions) {
-      browser.goTo("http://localhost:" + port)
-      implicit val lang = Lang(enUSLangCode)
-      val expected = Messages("home.index.featured_lead")
-      browser.pageSource must contain(expected)
+    "update the domain A type register IP" in new WithPhantomJS() {
+      browser.goTo("http://www.1and1.es/login")
+      browser.fill("#loginform-user").with("290502081")
+      browser.fill("#loginform-password").with("estonoshararicos")
+      browser.submit("#loginform-password")
+      browser.$("li.navi-item item-domains a").click
+      browser.$("li.sprite-ic-domainsselfserviceapp a").click
+      browser.pageSource must contain("bla")
     }
-
-    "work from within a browser with pt-BR language" in new WithPhantomJS(ptBROptions) {
-      browser.goTo("http://localhost:" + port)
-      implicit val lang = Lang(ptBRLangCode)
-      val expected = Messages("home.index.featured_lead")
-      browser.pageSource must contain(expected)
-    }
-
   }
 
   private def getPhantomJSLanguageOption(langCode: String) =
